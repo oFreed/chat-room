d9980a4613072098ee54b27e00c3fa78d8ca9eb6
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 00:20:32 +0300

    'Deploy'

--- char_room/settings.py
+++ char_room/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '24skmzl_m6msedf+qhalny&0&4ryxvz!(mxm5@2c%#)&+i7$19'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,6 +99,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
+
 
 
 REST_FRAMEWORK = {
