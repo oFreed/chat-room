0952fc599a9926232569c37b81f4747e66f520b9
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 21:43:51 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -99,12 +99,13 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 REST_FRAMEWORK = {
