7bf22cc9557bc53be2e5f2b5b0518534117d6999
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 10:59:41 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -103,7 +103,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-heroku ps:restart
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
