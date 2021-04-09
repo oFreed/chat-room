c16ecad45b846e3b0150c950e212e188c5acbcc3
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 10:35:03 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
-
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,15 +92,22 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
