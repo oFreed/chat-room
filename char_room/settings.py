1644b70e1729f3500e7785a77211bbc05abc5561
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 22:06:42 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -94,13 +94,13 @@ USE_TZ = True
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
