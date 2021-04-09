ecdebcf96dac80f9f82f2723b486114dd5c9fa90
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 10:57:48 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -103,8 +103,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+heroku ps:restart
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
