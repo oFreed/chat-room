780b367a6eac36b754365f32ca84efe0b6b6c2c0
Oleg Litvin <Nenya2003@gmail.com>
2021-04-09 21:47:36 +0300

    Deploy

--- char_room/settings.py
+++ char_room/settings.py
@@ -103,7 +103,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
